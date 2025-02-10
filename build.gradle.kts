aec03bb4a7b797afac8918b930ca38705db91cdd
xism4 <soportexism4@gmail.com>
2025-02-10 15:43:59 +0100

    feat(shadowJar)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.github.goooler.shadow") version "8.1.8" // Support Java 21
 }
 
 group = "es.xism4.software"
@@ -17,4 +18,33 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val targetJavaVersion = 17
+
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    archiveBaseName.set("i-balancer")
+    archiveVersion.set("0.1")
+    archiveClassifier.set("all")
+    mergeServiceFiles()
+
+    relocate("com.fazecast", "es.xism4.software.libs")
+    relocate("org.junit", "es.xism4.software.libs")
 }
